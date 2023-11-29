2fea4afdc12eeae028e275354fb5adf498a19ebd
Abbasov <MABBA952@marriott.com>
2023-11-29 17:17:33 +0400

    So tired of javascript

--- assets/javascript/hamburger.js
+++ assets/javascript/hamburger.js
@@ -1,50 +1,18 @@
-var links = document.querySelector("header nav ul li:nth-child(2)"),
-    joinLink = document.querySelector("#join_us");
-
-
-    function goster(){
-        links.style.display = "none";
-        joinLink.style.width = "60%";
-        joinLink.innerHTML = " ";
-
-        
-        var div = document.createElement("div");
-
-        div.innerHTML = `
-            <span class="span_ham">  </span>
-            <span class="span_ham">  </span>
-            <span class="span_ham">  </span>
-        `
-
-        joinLink.appendChild(div);
-    }
-
-    function gosterme(){
-        links.style.display = "flex";
-        links.style.position = "absolute";
-        links.style.top = "44px";
-        links.style.margin = "0 auto";
-        links.style.width = "110%";
-        links.style.columnGap = "5px";
-    }
+var links = document.querySelector('#links'),
+    hamburger = document.querySelector('#hamburger'),
+    sectionOne = document.querySelector('.sec_1');
 
 
 
-    if(window.screen.width < 1000){
-        joinLink.innerHTML = " ";
-        goster();
-        let a = 1;
-
-        joinLink.onclick = function(){
-            console.log(a);
-            if(a == 1){
-                goster();
-                a = 0;
-            }else if (a == 0){
-                gosterme();
-                a = 1;
-            }
+    hamburger.onclick = function() {
+    
+        if(sectionOne.style.marginTop === "180px"){
+            sectionOne.style.marginTop = "50px";
+        }else{
+            sectionOne.style.marginTop = "180px";
         }
-    }
 
+        links.classList.toggle('enable');
+        console.log(links.classList)
 
+    };
