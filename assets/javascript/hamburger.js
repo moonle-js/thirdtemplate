9db5bd6c3167734c4b62a783fd5715a9a2e4e302
Abbasov <MABBA952@marriott.com>
2023-12-09 11:01:42 +0400

    Join us sextion need to be followed up

--- assets/javascript/hamburger.js
+++ assets/javascript/hamburger.js
@@ -1,18 +1,27 @@
 var links = document.querySelector('#links'),
     hamburger = document.querySelector('#hamburger'),
-    sectionOne = document.querySelector('.sec_1');
+    sectionOne = document.querySelector('.sec_1'),
+    image_nav = document.querySelector('#image_nav');
 
 
 
     hamburger.onclick = function() {
     
-        if(sectionOne.style.marginTop === "180px"){
+        if(sectionOne.style.marginTop === "200px"){
             sectionOne.style.marginTop = "50px";
         }else{
-            sectionOne.style.marginTop = "180px";
+            sectionOne.style.marginTop = "200px";
         }
 
         links.classList.toggle('enable');
+        
+
+        if(links.classList.contains('enable')){
+            image_nav.style.display = "inline-block";
+        }else{
+            image_nav.style.display = "none";
+        }
+
         console.log(links.classList)
 
     };
